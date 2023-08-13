24764cfd51cc2ad29dc8aa2084c226f2978d721e
K4m0 <juan.kamo00@gmail.com>
2023-08-13 15:40:11 -0500

    Plan page almost done

--- src/pages/Plan.tsx
+++ src/pages/Plan.tsx
@@ -1,24 +1,16 @@
-import {
-  Avatar,
-  Box,
-  Card,
-  CardHeader,
-  Grid,
-  ToggleButton,
-  ToggleButtonGroup,
-} from "@mui/material";
-import { red } from "@mui/material/colors";
 import { useState } from "react";
 import arcadeImg from "../assets/arcade.svg";
 import advancedImg from "../assets/advanced.svg";
 import proImg from "../assets/pro.svg";
+import { Grid, Stack, Typography } from "@mui/material";
+import AntSwitch from "../components/AntSwitch";
 
 interface PlanProps {
   onSubmit: any;
 }
 
 const Plan = ({ onSubmit }: PlanProps) => {
-  const [alignment, setAlignment] = useState("web");
+  const [alignment, setAlignment] = useState("arcade");
 
   const handleChange = (
     event: React.MouseEvent<HTMLElement>,
@@ -30,42 +22,66 @@ const Plan = ({ onSubmit }: PlanProps) => {
   return (
     <div className="plan">
       <div>
-        <ToggleButtonGroup
-          color="primary"
-          value={alignment}
-          exclusive
-          onChange={handleChange}
-          aria-label="Platform"
-          className="toggleButtonGroup"
-        >
-          <ToggleButton className="planToggleButton" value="web">
-            <Avatar className="avatar">
-              <img src={arcadeImg} />
-            </Avatar>
+        <div className="toggleButtonGroup">
+          <button
+            className="planToggleButton"
+            value={alignment}
+            onChange={() => setAlignment("arcade")}
+          >
+            <img className="buttonImage" src={arcadeImg} />
             <div className="planTitle">Arcade</div>
-          </ToggleButton>
+            <div className="planSubTitle">$9/mo</div>
+          </button>
 
-          <ToggleButton className="planToggleButton" value="android">
-            <Avatar>
-              <img src={advancedImg} />
-            </Avatar>
-            <div>Advanced</div>
-          </ToggleButton>
+          <button
+            className="planToggleButton"
+            value={alignment}
+            onChange={() => setAlignment("advanced")}
+          >
+            <img className="buttonImage" src={advancedImg} />
 
-          <ToggleButton className="planToggleButton" value="ios">
-            <Avatar>
-              <img src={proImg} />
-            </Avatar>
-            <div>Pro</div>
-          </ToggleButton>
-        </ToggleButtonGroup>
+            <div className="planTitle">Advanced</div>
+            <div className="planSubTitle">$12/mo</div>
+          </button>
+
+          <button
+            className="planToggleButton"
+            value={alignment}
+            onChange={() => setAlignment("pro")}
+          >
+            <img className="buttonImage" src={proImg} />
+
+            <div className="planTitle">Pro</div>
+            <div className="planSubTitle">$15/mo</div>
+          </button>
+        </div>
+        <div className="planFrequency">
+          {/* <div className="monthly">Monthly</div>
+          <div>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round"></span>
+            </label>
+          </div>
+          <div className="yearly">Yearly</div> */}
+          <Stack direction="row" spacing={1} alignItems="center">
+            <Typography>Monthly</Typography>
+            <AntSwitch
+              defaultChecked
+              inputProps={{ "aria-label": "ant design" }}
+            />
+            <Typography>Yearly</Typography>
+          </Stack>
+        </div>
       </div>
-      <button className="goBackButton" type="submit">
-        Go Back
-      </button>
-      <button className="nextStepButton" type="submit">
-        Next Step
-      </button>
+      <div className="backNextButtons">
+        <button className="goBackButton" type="submit">
+          Go Back
+        </button>
+        <button className="nextStepButton" type="submit">
+          Next Step
+        </button>
+      </div>
     </div>
   );
 };
